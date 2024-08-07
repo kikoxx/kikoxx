0563532c299aab90a30a7bca4bd3084ea4d037d6
kiko <facadehat@gmail.com>
2024-08-08 01:03:49 +0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
-// const path = require('path');
+const path = require('path');
 
-// module.exports = {
-//   mode: 'production',
-//   entry: './src/index.js',
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     filename: 'bundle.js',
-//     publicPath: '/',  // Ensure this is set correctly for your deployment
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: ['@babel/preset-env', '@babel/preset-react'],
-//           },
-//         },
-//       },
-//       {
-//         test: /\.css$/,
-//         use: ['style-loader', 'css-loader'],
-//       },
-//     ],
-//   },
-//   resolve: {
-//     extensions: ['.js', '.jsx'],
-//   },
-// };
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',  // Ensure this is set correctly for your deployment
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
