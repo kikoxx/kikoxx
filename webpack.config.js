352da8926b7f22dd0c2ef1d010ca2ac5b88e9e7d
kiko <facadehat@gmail.com>
2024-08-07 18:44:11 +0800

    web update

--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,28 @@
-// const path = require('path');
-
-// module.exports = {
-//   mode: 'development',
-//   entry: './src/index.js',
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     filename: 'bundle.js'
-//   },
-//   devServer: {
-//     static: {
-//       directory: path.join(__dirname, 'public'),  // Use 'public' or the folder you want to serve static files from
-//     },
-//     compress: true,
-//     port: 9000,
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: ['@babel/preset-env', '@babel/preset-react'],
-//           },
-//         },
-//       },
-//       {
-//         test: /\.css$/,
-//         use: ['style-loader', 'css-loader'],
-//       },
-//     ],
-//   },
-// };
 const path = require('path');
 
-module.exports = (env, argv) => {
-  const isProduction = argv.mode === 'production';
-
-  return {
-    mode: isProduction ? 'production' : 'development',
-    entry: './src/index.js',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js',
-      publicPath: '/',
-    },
-    devServer: {
-      static: {
-        directory: path.join(__dirname, 'public'), // Serve static files from 'public' directory
-      },
-      compress: true,
-      port: 9000,
-      historyApiFallback: true, // Needed for React Router
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react'],
-            },
+module.exports = {
+  mode: 'production',  // Make sure this is set to production
+  entry: './src/index.js',  // Your main entry point
+  output: {
+    path: path.resolve(__dirname, 'dist'),  // Output directory
+    filename: 'bundle.js'  // Output file name
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           },
         },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        },
-      ],
-    },
-  };
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
